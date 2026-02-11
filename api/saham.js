031c6f67bbe6af746ae5c4941ccf0ef2e29f256c
muhsugandi <sugandigani@ymail.com>
2026-02-11 22:33:22 +0800

    Create saham.js

---
+++ api/saham.js
@@ -0,0 +1,45 @@
+export default async function handler(req, res) {
+
+    const symbol = req.query.symbol + ".JK";
+
+    try {
+
+        const response = await fetch(
+        `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=price,defaultKeyStatistics,financialData`
+        );
+
+        const json = await response.json();
+        const result = json.quoteSummary.result[0];
+
+        const price = result.price.regularMarketPrice.raw;
+        const eps = result.defaultKeyStatistics.trailingEps.raw;
+        const growth = result.financialData.earningsGrowth.raw * 100;
+
+        const per = price / eps;
+        const fairPrice = eps * growth;
+
+        let status = "";
+
+        if(price > fairPrice*1.2){
+            status = "SANGAT MAHAL";
+        } else if(price > fairPrice){
+            status = "MAHAL";
+        } else if(price < fairPrice*0.8){
+            status = "MURAH";
+        } else {
+            status = "WAJAR";
+        }
+
+        res.status(200).json({
+            price: price.toFixed(2),
+            eps: eps.toFixed(2),
+            per: per.toFixed(2),
+            growth: growth.toFixed(2),
+            fairPrice: fairPrice.toFixed(2),
+            status
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Data tidak tersedia." });
+    }
+}
